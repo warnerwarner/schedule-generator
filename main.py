a3ef0a6bc8dd19172d49c92a0168c571aba0459d
warnerwarner <tom.p.a.warner@gmail.com>
2019-08-27 14:10:41 +0100

    update python path in main

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import inspect
 import numpy as np
 import pickle
 
+
 from PyQt5 import QtWidgets
 from Designs import mainDesign
 from Models import Widgets
-
+sys.path.append('C:\\Users\\warnert\\Documents\\GitHub')
+sys.path.append('C:\\Users\\warnert\\Documents\\GitHub\\schedule-generator')
 from Models import ScheduleWidgets, ScheduleView
 from UI import ColorMap
 from PyPulse import PulseInterface
